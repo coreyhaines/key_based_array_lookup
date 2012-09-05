ee7e8e7c3e4d57f6c0b1db536bc6bd8885c6e6f2
coreyhaines <coreyhaines@gmail.com>
2012-09-05 14:41:18 +0200

    Adding a single cell with a location should then yield it up

--- lookup_spec.rb
+++ lookup_spec.rb
@@ -1,5 +1,22 @@
 class CellsByLocation
+  def initialize
+    @cells = []
+  end
+
   def at_location(location, &block)
+    @cells.each(&block)
+  end
+
+  def set(cell, location)
+    @cells << cell
+  end
+end
+
+class Cell
+end
+
+class Location
+  def initialize(x, y)
   end
 end
 
@@ -8,6 +25,18 @@ describe "Looking up cells by location" do
     it "does not call the callback" do
       cells = CellsByLocation.new
       expect { |b| cells.at_location(Object.new, &b) }.not_to yield_control
+    end
+  end
+
+  context "when a cell has been added" do
+    context "and you ask for it by its location" do
+      it "sends the cell to you" do
+        cells = CellsByLocation.new
+        cell = Cell.new
+        location = Location.new 1, 1
+        cells.set cell, location
+        expect { |b| cells.at_location(location, &b) }.to yield_with_args(cell)
+      end
     end
   end
 end
